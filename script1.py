7a1089d2828a7fd7f543ec891caf4b78473d0ee6
Vivan <vivanvatsa@gmail.com>
2020-09-17 22:07:01 +0530

    Update script1.py

--- script1.py
+++ script1.py
@@ -1,10 +1,16 @@
 import sqlite3
 
-conn = sqlite3.connect("lite.db")
-cur = conn.cursor()
-# cur.execute("CREATE TABLE store (item TEXT, quantity INTEGER, price REAL)")
-cur.execute(
-    "CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
-cur.execute("INSERT")
-conn.commit()
-conn.close()
+
+def create_table():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    # cur.execute("CREATE TABLE store (item TEXT, quantity INTEGER, price REAL)")
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
+    # cur.execute("INSERT INTO store VALUES ('wine glass',8,10.5)")
+    conn.commit()
+    conn.close()
+
+
+def insert():
+    cur.execute("INSERT INTO store VALUES ('wine glass',8,10.5)")
